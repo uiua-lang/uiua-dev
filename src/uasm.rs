8862f8b234cdc74a02e5811203ad811985b9010a
Kai Schmidt <kaikaliischmidt@gmail.com>
2024-07-10 10:18:35 -0700

    working on uasm parsing

--- src/uasm.rs
+++ src/uasm.rs
@@ -2,7 +2,7 @@ use std::{path::Path, sync::Arc};
 
 use ecow::EcoString;
 
-use crate::{CodeSpan, InputSrc, LexError, Loc, Sp};
+use crate::{Assembly, CodeSpan, InputSrc, Instr, LexError, Loc, ParseError, Sp};
 
 #[derive(Clone)]
 enum UasmToken {
@@ -18,7 +18,6 @@ struct Lexer {
     loc: Loc,
     src: Arc<Path>,
     tokens: Vec<Sp<UasmToken>>,
-    errors: Vec<Sp<LexError>>,
 }
 
 impl Lexer {
@@ -79,10 +78,41 @@ impl Lexer {
             match c {
                 ':' => self.end(UasmToken::Colon, start),
                 '\n' => self.end(UasmToken::Newline, start),
+                // String
+                '"' => {
+                    let mut s = EcoString::new();
+                    let mut escaped = false;
+                    while let Some(c) = self.next_char() {
+                        if escaped {
+                            match c {
+                                'n' => s.push('\n'),
+                                'r' => s.push('\r'),
+                                't' => s.push('\t'),
+                                '"' => s.push('"'),
+                                '\\' => s.push('\\'),
+                                _ => {
+                                    return Err(self
+                                        .end_span(start)
+                                        .sp(LexError::InvalidEscape(c.into())))
+                                }
+                            }
+                            escaped = false;
+                        } else if c == '\\' {
+                            escaped = true;
+                        } else if c == '"' {
+                            break;
+                        } else {
+                            s.push(c);
+                        }
+                    }
+                    self.end(UasmToken::String(s), start);
+                }
+                // Ident
                 c if c.is_alphabetic() => {
                     while self.next_char_if(char::is_alphabetic).is_some() {}
                     self.end(UasmToken::Ident, start);
                 }
+                // Number
                 c if c == '-' || c.is_ascii_digit() => {
                     while self.next_char_if(|c| c.is_ascii_digit()).is_some() {}
                     if self.next_char_exact('.') {
@@ -100,4 +130,69 @@ impl Lexer {
         }
         Ok(self.tokens)
     }
+}
+
+struct Parser<'a> {
+    input: &'a str,
+    curr: usize,
+    tokens: Vec<Sp<UasmToken>>,
+    errors: Vec<Sp<LexError>>,
+}
+
+impl<'a> Parser<'a> {
+    fn next_map<T>(&mut self, f: impl FnOnce(&UasmToken) -> Option<T>) -> Option<Sp<T>> {
+        let token = self.tokens.get(self.curr)?;
+        let value = f(&token.value)?;
+        self.curr += 1;
+        Some(token.span.clone().sp(value))
+    }
+    fn ident(&mut self) -> Option<Sp<&'a str>> {
+        let token = self.tokens.get(self.curr)?;
+        if !matches!(token.value, UasmToken::Ident) {
+            return None;
+        }
+        self.curr += 1;
+        Some(token.span.clone().sp(&self.input[token.span.byte_range()]))
+    }
+    fn uint(&mut self) -> Option<Sp<u64>> {
+        let token = self.tokens.get(self.curr)?;
+        if !matches!(token.value, UasmToken::Number) {
+            return None;
+        }
+        let n = self.input[token.span.byte_range()].parse().ok()?;
+        self.curr += 1;
+        Some(token.span.clone().sp(n))
+    }
+    fn number(&mut self) -> Option<Sp<f64>> {
+        let token = self.tokens.get(self.curr)?;
+        if !matches!(token.value, UasmToken::Number) {
+            return None;
+        }
+        self.curr += 1;
+        let n = self.input[token.span.byte_range()].parse().unwrap();
+        Some(token.span.clone().sp(n))
+    }
+    fn instr(&mut self) -> Option<Sp<Instr>> {
+        let ident = self.ident()?;
+        Some(ident.span.sp(match ident.value {
+            _ => return None,
+        }))
+    }
+}
+
+pub fn parse(input: &str, path: &Path) -> Result<Assembly, Vec<Sp<ParseError>>> {
+    let lexer = Lexer {
+        chars: input.chars().collect(),
+        loc: Loc::default(),
+        src: path.into(),
+        tokens: Vec::new(),
+    };
+    let tokens = lexer.run().map_err(|e| vec![e.map(ParseError::Lex)])?;
+    let mut parser = Parser {
+        input,
+        curr: 0,
+        tokens,
+        errors: Vec::new(),
+    };
+    todo!()
 }
